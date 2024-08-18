35b48f2d7d22f36be30acce95c7b0b321704e0e5
jatin192003 <jatin192003@gmail.com>
2024-08-18 16:42:20 +0530

    add refreshAccessToken endpoint & bugfixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -58,7 +58,7 @@ userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
 }
 
-userSchema.methods.generateAccessToken = function () {
+userSchema.methods.generateAccessToken = function(){
     return jwt.sign(
         {
             _id: this._id,
@@ -72,11 +72,11 @@ userSchema.methods.generateAccessToken = function () {
         }
     )
 }
-
-userSchema.methods.generateRefreshToken = function () {
+userSchema.methods.generateRefreshToken = function(){
     return jwt.sign(
         {
             _id: this._id,
+            
         },
         process.env.REFRESH_TOKEN_SECRET,
         {
@@ -84,5 +84,4 @@ userSchema.methods.generateRefreshToken = function () {
         }
     )
 }
-
 export const User = mongoose.model("User", userSchema)
